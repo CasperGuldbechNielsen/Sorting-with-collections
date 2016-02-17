a516b91facb52aa64e2a81fb353a2663120f9e25
CasperGuldbechNielsen <casp674d@edu.easj.dk>
2016-02-17 10:27:05 +0100

    Initial commit

---
+++ Sorting with collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_with_collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
